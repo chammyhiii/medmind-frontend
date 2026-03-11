4dd411dbddd4d549c9a2b753d18172f0bf4812fe
chammyhiii <162902874+chammyhiii@users.noreply.github.com>
2026-03-11 21:57:36 +0700

    đổi

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/medmind-frontend/', // QUAN TRỌNG: Để GitHub Pages load đúng link
-})
+{
+  "version": 2,
+  "builds": [
+    { "src": "backend/server.js", "use": "@vercel/node" },
+    { "src": "frontend/**", "use": "@vercel/static" }
+  ],
+  "rewrites": [
+    {
+      "source": "/api/(.*)",
+      "destination": "/backend/server.js"
+    },
+    {
+      "source": "/(.*)",
+      "destination": "/frontend/$1"
+    }
+  ]
+}
